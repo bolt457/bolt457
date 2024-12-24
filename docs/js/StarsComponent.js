5e6c8d6dd50ab626795edc274a90afa6eb7bbbe5
Kraibouet <giselleboltronne@gmail.com>
2024-12-24 02:08:08 +0100

    Mise à jour et correction des fichiers JavaScript et HTML

--- docs/js/StarsComponent.js
+++ docs/js/StarsComponent.js
@@ -1,17 +1,18 @@
+// StarsComponent.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './components/Dashboard';
-import SearchBar from './components/SearchBar';
-import InvestButton from './components/InvestButton';
 
-function StarsComponent() {
-    return (
-        <div className="App">
-            <SearchBar />
-            <Dashboard />
-            <InvestButton starId={1} />
-        </div>
-    );
+class StarsComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Les Stars</h2>
+                <p>Voici la liste des stars...</p>
+                {/* Ajoute ici la logique pour afficher les stars */}
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<StarsComponent />, document.getElementById('root'));
